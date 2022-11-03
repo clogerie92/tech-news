10d9bc050d12d92f7091b36226aeb6bb51d49dd1
clogerie <66565287+clogerie@users.noreply.github.com>
2022-11-03 10:47:35 -0400

    added VariableTypes class

---
+++ tech-news-java-api/VariableTypes.java
@@ -0,0 +1,16 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra";
+        int age = 2016 + 69;
+        double price = 1999.99;
+        boolean truth = true;
+        boolean falseness = !truth;
+
+        System.out.println("Her name is" + name + "!");
+        System.out.println("Her age is" + age + "!");
+        System.out.println("Her price is" + price + "!");
+        System.out.println("This is boolean is true: " + truth);
+        System.out.println("This is boolean is true: " + falseness);
+    }
+
+}
